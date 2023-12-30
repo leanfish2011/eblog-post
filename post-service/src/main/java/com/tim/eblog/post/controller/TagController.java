adc0eb54936fcd1568eb7e1e5151028b06ccda29
yufei <yangtze_yufei@163.com>
2023-12-30 17:11:32 +0800

    feat:增加归档接口

--- post-service/src/main/java/com/tim/eblog/post/controller/TagController.java
+++ post-service/src/main/java/com/tim/eblog/post/controller/TagController.java
@@ -18,20 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Api(tags = "标签管理")
 @RestController
-@RequestMapping("${api.version.module}/blog")
+@RequestMapping("${api.version.module}/blog/tag")
 public class TagController {
 
   @Autowired
   private TagService tagService;
 
   @ApiOperation(value = "查看博客全部标签")
-  @RequestMapping(value = "/tag", method = RequestMethod.GET)
+  @RequestMapping(method = RequestMethod.GET)
   public Message<List<String>> listTag() {
     return Message.success(tagService.listTag());
   }
 
   @ApiOperation(value = "查看标签下的博客")
-  @RequestMapping(value = "/tag/blog", method = RequestMethod.GET)
+  @RequestMapping(value = "/blog", method = RequestMethod.GET)
   public Message<List<TagBlogData>> queryTagBlog() {
     return Message.success(tagService.queryTagBlog());
   }
